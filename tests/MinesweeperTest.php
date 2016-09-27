992d681c180fec38789bca8eddb9570fce1ef9b2
Fafan <novareza@ralali.com>
2016-09-27 15:10:49 +0700

    add MinesweeperTest

---
+++ tests/MinesweeperTest.php
@@ -0,0 +1,21 @@
+<?php
+
+class MinesweeperTest extends TestCase
+{
+    /**
+     * testElement : test if element existed and has the expected value
+     *
+     * @return void
+     */
+    public function testCells()
+    {
+        echo "\n* MinesweeperTest::testCells()";
+        $dom = new DomDocument();
+        $dom->loadHTML( $this->visit('/minesweeper')->crawler->html() );
+        $this->assertEquals($dom->getElementById('title')->textContent, 'Minesweeper');
+        for ($x=1; $x<=8; $x++)
+            for ($y=1; $y<=8; $y++)
+                $this->assertEquals($dom->getElementById('cell-'.$x.'x'.$y)->textContent, 'cek');
+
+    }
+}
