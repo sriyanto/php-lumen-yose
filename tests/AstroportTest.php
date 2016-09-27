0ee9954be8d51132a3458297869fc4ffddccec25
Fafan <novareza@ralali.com>
2016-09-27 16:00:45 +0700

    add astroport test

--- tests/AstroportTest.php
+++ tests/AstroportTest.php
@@ -13,5 +13,17 @@ class AstroportTest extends TestCase
         $dom = new DomDocument();
         $dom->loadHTML( $this->visit('/astroport')->crawler->html() );
         $this->assertEquals($dom->getElementById('astroport-name')->textContent, 'Astroport Name');
+        $gate1 = (null !== $dom->getElementById('gate-1'));
+        $gate2 = (null !== $dom->getElementById('gate-2'));
+        $gate3 = (null !== $dom->getElementById('gate-3'));
+        $this->assertEquals($gate1, true);
+        $this->assertEquals($gate2, true);
+        $this->assertEquals($gate3, true);
+        $ship1 = (null !== $dom->getElementById('ship-1'));
+        $ship2 = (null !== $dom->getElementById('ship-2'));
+        $ship3 = (null !== $dom->getElementById('ship-3'));
+        $this->assertEquals($ship1, true);
+        $this->assertEquals($ship2, true);
+        $this->assertEquals($ship3, true);
     }
 }
