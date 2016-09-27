016601845eb4ce2587e8733de7e6b158ae2887ab
Fafan <novareza@ralali.com>
2016-09-27 15:52:12 +0700

    Fix minesweeper failed test

--- resources/views/astroport.blade.php
+++ resources/views/astroport.blade.php
@@ -1,1 +1,19 @@
-<div id="astroport-name">YoseThegame</div>
+<div id="astroport-name" class="astroport-name">Astroport Name</div>
+<div id="gate-1" class="gate-1 free">
+    Gate 1
+    <div id="ship-1">
+        Ship 1
+    </div>
+</div>
+<div id="gate-2">
+    Gate 2
+    <div id="ship-2">
+        Ship 2
+    </div>
+</div>
+<div id="gate-3">
+    Gate 3
+    <div id="ship-3">
+        Ship 3
+    </div>
+</div>
