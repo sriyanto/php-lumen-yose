9b2de71bb9e7b618c6420cf27ea8cf630804540d
Anggi Susanto <anggi.susanto@ralali.com>
2016-09-27 15:10:31 +0700

    add click

--- resources/views/minesboard.blade.php
+++ resources/views/minesboard.blade.php
@@ -7,7 +7,7 @@ for($i =1; $i<=$x;$i++){
 	$y = 8;
   	for($ii = 1; $ii<=$y;$ii++){
   		if($i == 3 && $ii==6){
-  			echo '<td id="cell-'.$i.'x'.$ii.'">bomb</td>';
+  			echo '<td id="cell-'.$i.'x'.$ii.'" onclick="ganti(this)">empty</td>';
   		}else{
   			echo '<td id="cell-'.$i.'x'.$ii.'">empty</td>';
   		}
@@ -18,8 +18,15 @@ for($i =1; $i<=$x;$i++){
 </table>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script type="text/javascript">
-	$('#cell-3x6').click(function(){
-		console.log('click');
-	  $('#cell-3x6').text('lost')
-	});
+	// $(body).load(function(){
+	// 	$('#cell-3x6').text('empty');
+	// })
+	function ganti(elem){
+	  $(elem).text('lost');
+
+	}
+	// $('#cell-3x6').click(function(){
+	// 	console.log('click');
+	//   $('#cell-3x6').text('lost');
+	// });
 </script>
