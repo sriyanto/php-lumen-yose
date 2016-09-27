31928ecf61444f27780cde70f62c117f0ff54a3f
Anggi Susanto <anggi.susanto@ralali.com>
2016-09-27 14:48:40 +0700

    data injection

--- resources/views/minesboard.blade.php
+++ resources/views/minesboard.blade.php
@@ -6,9 +6,20 @@ for($i =1; $i<=$x;$i++){
   	echo "<tr>";
 	$y = 8;
   	for($ii = 1; $ii<=$y;$ii++){
-      	echo '<td id="cell-'.$i.'x'.$ii.'">cek</td>';
+  		if($i == 3 && $ii==6){
+  			echo '<td id="cell-'.$i.'x'.$ii.'">bomb</td>';
+  		}else{
+  			echo '<td id="cell-'.$i.'x'.$ii.'">empty</td>';
+  		}
     }
   	echo "</tr>";
 }
 ?>
 </table>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript">
+	$('#cell-3x6').click(function(){
+		console.log('click');
+	  $('#cell-3x6').text('lost')
+	});
+</script>
